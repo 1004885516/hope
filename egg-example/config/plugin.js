fc15a111770b4ddb476122492dfd62fbcd3331da
张献秋 <zhangxianqiu>
2020-04-04 11:15:37 +0800

    mongoose配置

--- egg-example/config/plugin.js
+++ egg-example/config/plugin.js
@@ -1,12 +1,7 @@
 'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-};
+
 exports.cors = {
     enable: true,
     package: 'egg-cors',
