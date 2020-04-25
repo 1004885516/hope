2764f68031012044caf6f85c9f9c7be5692e9e07
张献秋 <1004885516@qq.com>
2020-04-25 18:52:34 +0800

    feat(all): 规范代码

--- egg-example/app/middleware/verify_user.js
+++ egg-example/app/middleware/verify_user.js
@@ -1,25 +1,44 @@
 'use strict';
+
+
 const jwt = require('jsonwebtoken');
 const common = require('../common');
 const { SystemError } = common
 const { TOKEN_ERR, NO_DATA_ERR } = common.Constant.ERR_CODE.ERR_CODE
+
 async function verifyUser(ctx, next) {
+
     ctx.logger.info('verifyUser')
+
     const token = ctx.request.body['x-access-token'] || ctx.request.headers['x-access-token'];
+
     if(token){
+
         const userdata = jwt.verify(token, ctx.app.config.secret_keys.usersecret)
-        const user = await ctx.model.User.findOne({ login: userdata.login })
+        // const user = await ctx.model.User.findOne({ login: userdata.login })
+        const user = {};
         if(!user){
-            throw new SystemError({ code: NO_DATA_ERR, message: "未找到该用户" })
+
+            const errorObj = { code: NO_DATA_ERR, message: "未找到该用户" }
+
+            const error = new SystemError(errorObj);
+            console.log('error####', error)
+            throw(error)
         }
+
         ctx.user = user // 把user信息挂载到ctx对象上，方便以后在业务代码中直接获取用户信息，节省部分数据库查询操作
+
         await next();
+
     }else{
-        ctx.throw(new SystemError({ code: TOKEN_ERR, message: `no_tokeno \n url: ${ ctx.request.url }`}))
+        const error = new SystemError({ code: TOKEN_ERR, message: `no_tokeno \n url: ${ ctx.request.url }`})
+        throw(error)
     }
 };
 function getValidator(type){
+
     let validatorFunc;
+
     switch(type){
         case 'verifyUser':
             validatorFunc = verifyUser
@@ -27,6 +46,7 @@ function getValidator(type){
         default:
             validatorFunc = null;
     }
+
     return validatorFunc;
 }
 
