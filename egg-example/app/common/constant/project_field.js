4363c557bbcae026ec4c8b54ca20a886f8df83e9
张献秋 <1004885516@qq.com>
2020-06-24 14:39:18 +0800

    fix(project_field):上传图片url修复

--- egg-example/app/common/constant/project_field.js
+++ egg-example/app/common/constant/project_field.js
@@ -2,8 +2,8 @@
 
 
 const env = process.env.NODE_ENV;
-const upload_path = env === 'env' ? 'D:/nginx/nginxServer/static' : '/root/nginx/upload';
-const upload_url = env === 'env' ? 'http://127.0.0.1:8089' : '60.205.191.215'
+const upload_path = env === 'development' ? 'D:/nginx/nginxServer/static' : '/root/nginx/upload';
+const upload_url = env === 'development' ? 'http://127.0.0.1:8089' : '60.205.191.215/upload'
 
 exports.PROJECT_FIELD = {
 
